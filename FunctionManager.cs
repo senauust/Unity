8ee0e16fda68ee94e7a7e6ec85d1517dd272b611
Sena Üst <61234188+senauust@users.noreply.github.com>
2020-08-18 18:13:21 +0300

    FunctionManager
    Simple examples

---
+++ FunctionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionManager : MonoBehaviour
+{
+    //public or private void function's name
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start is working");
+        MyFunction ();
+        Debug.Log("Line after my function");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MyFunction()
+    {
+        Debug.Log("This part is the part inside my function");
+    }
+
+
+
+
+}
