0e9bbdb76f6a6e7aad8db366aa1f8d565f43a59d
Sena Üst <61234188+senauust@users.noreply.github.com>
2020-08-18 18:29:20 +0300

    Returning Vector 3 Arrays from a Function

---
+++ Return.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionManager : MonoBehaviour
+{
+    //public or private void function's name
+    // Start is called before the first frame update
+    public Game Object[] player ;
+
+    private void Start()
+    {
+       player = GetAllThePlayers();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    GameObject [] GetAllThePlayers()
+    {
+        GameObject [] AllThePlayer = GameObject.FindGamesWithTag("Player");
+
+        foreach (var p in AllThePlayer)
+        {
+            p.GetComponent<MeshRenderer>().material.color = new Color (Random.value , Random.value , Random.value);
+        }
+    return AllThePlayer;
+    
+    
+    }
+
+
+
+}
